c406e4eb52bbced54cc39f129f2ecc6b3fe75fea
ANURAGTIWARIG <144209109+ANURAGTIWARIG@users.noreply.github.com>
2023-09-06 12:11:27 +0530

    Add files via upload

---
+++ A1 (1).java
@@ -0,0 +1,10 @@
+// ANURAG TIWARI
+import java.util.Scanner;
+public class A1{
+    public static void main(String[]args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        System.out.print("the number is"+n);
+    }
+    
+}
